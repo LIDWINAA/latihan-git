c1a4c998451c5d75586c1e124493806b5c9ccd60
LIDWINAA <priscalidwina@gmail.com>
2021-10-15 22:54:48 -0700

    tugas pertemuan 2

---
+++ app/Views/halaman/daftar_member.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Daftar member</title>
+    </head>
+    <body>
+        <h1>hai. . kamu mau menjadi member?</h1>
+        <p1>Isikan data diri kamu ya agar kami kenal dengan member kami</p1>
+    </body>
+
+</html>
